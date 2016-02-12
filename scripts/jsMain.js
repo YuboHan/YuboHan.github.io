98b7a3b1ad958eb121c29f8f650fa5c757628400
YuboHan <hanyubo95@gmail.com>
2016-02-11 17:23:53 -0800

    quality of life visual changes

--- scripts/jsMain.js
+++ scripts/jsMain.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 	/*var app = angular.module('myWebsite', []);
 
 	app.controller("HeaderController", function() {
@@ -7,15 +7,14 @@
 			return (this.selected == x);
 		};
 	}); */
-
-	var welcomePageHeight;
-	var aboutMePos;
-	var skillsPos;
-	var experiencesPos;
-	var projectsPos;
-	var contactMePos;
-	var scrollYPos;
-	var temp;
+    
+    "use strict";
+    /*jslint browser: true*/
+    /*global $, jQuery, alert*/
+    /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+    /*global define */
+    
+	var welcomePageHeight, aboutMePos, skillsPos, experiencesPos, projectsPos, contactMePos, scrollYPos, temp;
 
 	function resetNavigationBar() {
 		$(".navigation-block").css("background-color", "");
@@ -31,7 +30,7 @@
 	}
 
 	function adjustNavBar() {
-		if ( $(window).width() > 1183 ) {
+		if ($(window).width() > 1183) {
 			$("#navigation").css("left", "88.16568%");
 			$("#navigation").css("right", "auto");
 		} else {
@@ -41,108 +40,109 @@
 	}
 
 	function adjustWelcomePage() {
-		if ( $(window).width() > 1183 ) {
-			$("#welcome-name").css("margin-left","-550px");
-			$("#welcome-menu").css("margin-left","150px");
-		} else if ( $(window).width() > 974 ){
-			$("#welcome-name").css("margin-left","-375px");
-			$("#welcome-menu").css("margin-left","-25px");
+		if ($(window).width() > 1183) {
+			$("#welcome-name").css("margin-left", "-550px");
+			$("#welcome-menu").css("margin-left", "150px");
+		} else if ($(window).width() > 974) {
+			$("#welcome-name").css("margin-left", "-375px");
+			$("#welcome-menu").css("margin-left", "-25px");
 		} else {
-			$("#welcome-name").css("margin-left","-200px");
+			$("#welcome-name").css("margin-left", "-200px");
 		}
 	}
 
-	$(function(){
+	$(function () {
 		getYPosOfDivs();
 		adjustNavBar();
 		adjustWelcomePage();
 
-		$(".welcome-menu-button-text").hover(function() {
-			$(this).css("color", "red");
-		}, function(){
-			$(this).css("color", "#3D1F00");
+		$(".welcome-menu-button-text").hover(function () {
+			$(this).css("color", "#B4D235");
+		}, function () {
+			$(this).css("color", "whitesmoke");
 		});
 
-		$(".navigation-block-text").hover(function() {
-			$(this).css("color", "grey");
-		}, function() {
+		$(".navigation-block-text").hover(function () {
+			$(this).css("color", "#B4D235");
+		}, function () {
 			$(this).css("color", "white");
 		});
 
-		$(".welcome-about-me").click(function(e) {
-        	e.preventDefault();
-        	$("#about-me").scrollintoview({ duration: "normal"});
+		$(".welcome-about-me").click(function (e) {
+            e.preventDefault();
+            $("#about-me").scrollintoview({ duration: "normal"});
 		});
 
-		$(".welcome-skills").click(function(e) {
-        	e.preventDefault();
-        	$("#skills").scrollintoview({ duration: "normal"});
+		$(".welcome-skills").click(function (e) {
+            e.preventDefault();
+            $("#skills").scrollintoview({ duration: "normal"});
 		});
 
-		$(".welcome-experiences").click(function(e) {
-        	e.preventDefault();
-        	$("#experiences").scrollintoview({ duration: "normal"});
+		$(".welcome-experiences").click(function (e) {
+            e.preventDefault();
+            $("#experiences").scrollintoview({ duration: "normal"});
 		});
 
-		$(".welcome-projects").click(function(e) {
-        	e.preventDefault();
-        	$("#projects").scrollintoview({ duration: "normal"});
+		$(".welcome-projects").click(function (e) {
+            e.preventDefault();
+            $("#projects").scrollintoview({ duration: "normal"});
 		});
 
-		$(".welcome-contact-me").click(function(e) {
-        	e.preventDefault();
-        	$("#contact-me").scrollintoview({ duration: "normal"});
+		$(".welcome-contact-me").click(function (e) {
+            e.preventDefault();
+            $("#contact-me").scrollintoview({ duration: "normal"});
 		});
 
-		$(window).on("resize", function(event){
+		$(window).on("resize", function (event) {
 			console.log($(window).width());
 			getYPosOfDivs();
 			adjustNavBar();
 			adjustWelcomePage();
 		});
 
-		$(window).scroll(function() {
+		$(window).scroll(function () {
 			/* TODO: Right now if we remove this, there will be an error if you refresh page too much*/
 			getYPosOfDivs();
 			scrollYPos = $(document).scrollTop();
 			if (scrollYPos < welcomePageHeight) {
 				$("#dropdown").css({
-					'position':'absolute',
-					'top':'100%'
+					'position': 'absolute',
+					'top': '100%'
 				});
 			} else {
 				$("#dropdown").css({
-					'position':'fixed',
-					'top':'0%'
+					'position': 'fixed',
+					'top': '0%'
 				});
 			}
 			if (scrollYPos < welcomePageHeight - 50) {
 				$("#navigation").css({
-					'position':'absolute',
-					'top':'100%'
+					'position': 'absolute',
+					'top': '100%',
 				});
 			} else {
 				$("#navigation").css({
-					'position':'fixed',
-					'top':'50px'
+					'position': 'fixed',
+					'top': '50px',
+                    
 				});
 			}
 			if (scrollYPos < skillsPos - $(window).height() * 0.3) {
 				resetNavigationBar();
-				$("#navigation-about-me").css("background-color", "#FF704D");
+				$("#navigation-about-me").css("background-color", "#088DC5");
 			} else if (scrollYPos < experiencesPos - $(window).height() * 0.3) {
 				resetNavigationBar();
-				$("#navigation-skills").css("background-color", "#FF704D");
+				$("#navigation-skills").css("background-color", "#088DC5");
 			} else if (scrollYPos < projectsPos - $(window).height() * 0.3) {
 				resetNavigationBar();
-				$("#navigation-experiences").css("background-color", "#FF704D");
+				$("#navigation-experiences").css("background-color", "#088DC5");
 			} else if (scrollYPos < contactMePos - $(window).height() * 0.3) {
 				resetNavigationBar();
-				$("#navigation-projects").css("background-color", "#FF704D");
+				$("#navigation-projects").css("background-color", "#088DC5");
 			} else {
 				resetNavigationBar();
-				$("#navigation-contact-me").css("background-color", "#FF704D");
+				$("#navigation-contact-me").css("background-color", "#0694D2");
 			}
 		});
 	});
-})();
+}());
